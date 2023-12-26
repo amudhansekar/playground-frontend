b413697c74f8b9241ef575105985f18db84eb9b9
Kevin Lee <kevlee2198@gmail.com>
2023-12-25 21:46:18 -0500

    added nextui theming

--- playground-frontend/app/layout.tsx
+++ playground-frontend/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <main>
+          <main className="light text-foreground bg-background">
             <PlaygroundNavbar />
             {children}
             <Footer />
