409b1e41e07b96302f2fe4c40200c6fba8bc5302
Kevin Lee <kevlee2198@gmail.com>
2024-03-24 12:55:50 -0500

    team and player stuff

---
+++ src/player/model/player-api-response-full-dto.ts
@@ -0,0 +1,15 @@
+interface PlayerApiResponseFullDto {
+  id: number;
+
+  firstName: string;
+
+  lastName: string;
+
+  age?: number;
+
+  height?: number;
+
+  weight?: number;
+}
+
+export default PlayerApiResponseFullDto;
---
+++ src/team/model/team-instance-fields.ts
@@ -0,0 +1,23 @@
+// Fields shared between request and response
+const idField = "id";
+
+const nameField = "name";
+
+const descriptionField = "description";
+
+const attributesField = "attributes";
+
+// Fields for request
+const playerIdsField = "playerIds";
+
+// Fields for response
+const playersField = "players";
+
+export {
+  attributesField,
+  descriptionField,
+  idField,
+  nameField,
+  playerIdsField,
+  playersField,
+};
